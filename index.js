d52293e0637d27ede24c59125a2fd4a1f768ec0f
Jaime Zuspann <jaimeadele@gmail.com>
2017-10-07 07:45:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ const katzDeli = [];
 
 function takeANumber(katzDeliLine, name){
   katzDeli.push(name);
+  let place = katzDeli.indexOf(name) + 1;
+  return `Welcome, ${name}. You are number ${place} in line.`
 }
